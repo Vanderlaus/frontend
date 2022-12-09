a6f5fafcca04b7464296ec5869e9a3429fd5f74a
Vander Luis Lauschner <vander.lauschner@proway.treina>
2022-12-08 21:46:39 -0300

    subindo

--- modulo04/main.py
+++ modulo04/main.py
@@ -1,22 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from pessoa import Pessoa
 
-#
+pessoa1 = Pessoa('Haiko', '15', '1.50')
+pessoa2 = Pessoa('Jean', '42', '1.84')
+pessoa3 = Pessoa('Gisele', '16', '1.56')
+
+lista = [pessoa1, pessoa2, pessoa3]
+
 app = Flask(__name__)
 
-#
 @app.route('/')
 def inicio():
-    pessoa1 = Pessoa('haiko', '15', '1:50')
-    pessoa2 = Pessoa('Jean', '42', '1:84')
-    pessoa3 = Pessoa('Gisele', '16', '1:56')
-
-    lista = [pessoa1, pessoa2, pessoa3]
-    return render_template('index.html', titulo = 'Vandersão', pessoas = lista)
+    return render_template('index.html', titulo = 'Lista Pessoas', pessoas = lista)
 
 @app.route('/novo')
 def novo():
-    return 'Titulo Rota nova'
+    return render_template('novo.html', titulo = 'Cadastro Pessoa')
 
-
-app.run()
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    idade = request.form['idade']
+    altura = request.form['altura']
+    
+    abacate = Pessoa(nome, idade, altura)
+    
+    lista.append(abacate)
+    
+    return redirect('/')
+    
+app.run(debug=True)
